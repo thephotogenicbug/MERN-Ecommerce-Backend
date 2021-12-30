863c16d48b7f1e094bdb079f48a8ad35153779ef
thephotogenicbug <navinkumarachari@gmail.com>
2021-12-30 14:22:15 +0530

    MERN : Ecommerce backend
    initialize backend server for react ecommerce webapp and create signup component and gen salt (hashpassword) and save in mongoDB Database

---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection success");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connectDB;
